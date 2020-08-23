891a9e53b9d613157470afa38114e21f38cae656
Travis Fischer <fisch0920@gmail.com>
2020-08-23 08:00:46 -0400

    init

---
+++ index.js
@@ -0,0 +1,75 @@
+'use strict'
+
+const sharp = require('sharp')
+const pMap = require('p-map')
+
+/**
+ * @name lqipModern
+ *
+ * @param {Buffer|string|Buffer[]|string[]} input - Either an array of image inputs or a single image input. Each image input may either be a `Buffer` containing raw image data, or a `string` containing the filesystem path to a supported image type.
+ * @param {Object} [opts] - Optional configuration options.
+ * @param {number} [opts.concurrency=4] - Concurrency when processing an array of input images.
+ * @param {string} [opts.outputFormat='webp'] - Output format to use; either `webp` or `jpeg` (passing `jpg` is the same as passing `jpeg`).
+ * @param {Object} [opts.outputOptions] - Output options passed to either `sharp.webp` or `sharp.jpeg` dependent on `opts.outputFormat`.
+ * @param {number|any[]} [opts.resize] - Options to pass to `sharp.resize`. Defaults to resizing inputs to a max dimension of `60`, with the other dimension being calculated to maintain aspect ratio. If you want more control, you can pass an array of args here which will be forwarded to `sharp.resize`.
+ */
+module.exports = async function lqipModern(input, opts = {}) {
+  const { concurrency = 4, ...rest } = opts
+
+  if (Array.isArray(input)) {
+    return pMap(input, async (image) => computeLqipImage(image, rest), {
+      concurrency
+    })
+  } else {
+    return computeLqipImage(input, opts)
+  }
+}
+
+async function computeLqipImage(input, opts = {}) {
+  const { resize = 60, outputFormat = 'webp', outputOptions } = opts
+
+  const image = sharp(input)
+  const metadata = await image.metadata()
+
+  // by default, the resizing ensure that we won't ever upscale an image
+  const resized = image.resize(
+    ...(Array.isArray(resize)
+      ? resize
+      : [
+          Math.min(metadata.width, resize),
+          Math.min(metadata.height, resize),
+          { fit: 'inside' }
+        ])
+  )
+  let output
+
+  if (outputFormat === 'webp') {
+    output = resized.webp({
+      quality: 20,
+      alphaQuality: 20,
+      smartSubsample: true,
+      ...outputOptions
+    })
+  } else if (outputFormat === 'jpg' || outputFormat === 'jpeg') {
+    output = resized.jpeg({
+      quality: 20,
+      ...outputOptions
+    })
+  } else {
+    throw new Error(`Invalid outputformat "${outputFormat}"`)
+  }
+
+  const { data, info } = await output.toBuffer({ resolveWithObject: true })
+
+  return {
+    content: data,
+    metadata: {
+      originalWidth: metadata.width,
+      originalHeight: metadata.height,
+      width: info.width,
+      height: info.height,
+      type: outputFormat,
+      dataURIBase64: `data:image/webp;base64,${data.toString('base64')}`
+    }
+  }
+}
---
+++ index.test.js
@@ -0,0 +1,58 @@
+'use strict'
+
+const fs = require('fs-extra')
+const path = require('path')
+const test = require('ava')
+const globby = require('globby')
+const lqip = require('.')
+
+const fixtures = globby.sync('fixtures/*.{jpg,jpeg,webp,png}')
+const outputDir = 'output'
+
+test.before(async () => {
+  await fs.ensureDir(outputDir)
+})
+
+for (const fixture of fixtures) {
+  const name = fixture.split('/').pop().split('.').shift()
+
+  test(`${fixture} => webp`, async (t) => {
+    const result = await lqip(fixture)
+    t.truthy(result)
+    t.true(Buffer.isBuffer(result.content))
+    t.true(result.metadata.width < result.metadata.originalWidth)
+    t.true(result.metadata.height < result.metadata.originalHeight)
+
+    await fs.writeFile(path.join(outputDir, `${name}.webp`), result.content)
+
+    const { dataURIBase64, ...metadata } = result.metadata
+    console.log(metadata)
+    t.snapshot(metadata)
+  })
+
+  test(`${fixture} => jpeg`, async (t) => {
+    const result = await lqip(fixture, { outputFormat: 'jpeg' })
+    t.truthy(result)
+    t.true(Buffer.isBuffer(result.content))
+    t.true(result.metadata.width < result.metadata.originalWidth)
+    t.true(result.metadata.height < result.metadata.originalHeight)
+
+    await fs.writeFile(path.join(outputDir, `${name}.jpg`), result.content)
+
+    const { dataURIBase64, ...metadata } = result.metadata
+    console.log(metadata)
+    t.snapshot(metadata)
+  })
+}
+
+test(`array of inputs`, async (t) => {
+  const results = await lqip(fixtures)
+  t.true(Array.isArray(results))
+  t.is(results.length, fixtures.length)
+
+  for (const result of results) {
+    t.true(Buffer.isBuffer(result.content))
+    t.true(result.metadata.width < result.metadata.originalWidth)
+    t.true(result.metadata.height < result.metadata.originalHeight)
+  }
+})
